2707fb670de7ed3f8b0d315bb4c8e2bfab74fe01
}Camila Aparicio <camy47lg@gmail.com>
2025-10-16 13:34:22 -0300

    resolución de errores de mayúsculas

--- routes/estudiantes.js
+++ routes/estudiantes.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const router = express.Router();
 const Estudiante = require('../models/Estudiante');
-const e = require('express');
   
 //GET: obtener estudiantes, con filtro opcional por curso
 router.get('/estudiantes', async (req, res) => {
@@ -9,11 +8,6 @@ router.get('/estudiantes', async (req, res) => {
     const { curso } = req.query;
     const filtro = curso ? { cursos: { $in: [curso] } } : {};
     const estudiantes = await Estudiante.find(filtro);
-    if (curso) {
-      estudiantes = await Estudiante.find({ curso });
-    }else {
-      estudiantes = await Estudiante.find();
-    }
     res.json(estudiantes);
   } catch (err) {
     res.status(500).json({ error: 'Error al obtener estudiantes' });
@@ -23,9 +17,9 @@ router.get('/estudiantes', async (req, res) => {
 //Get: obtener estudiantes por ID
 router.get('/estudiantes/:id', async (req, res) => {
   try {
-    const estudiante = await Estudiante.findById(req.params.id);
-    if (!estudiante) return res.status(404).json({ error: 'Estudiante no encontrado' });
-    res.json(estudiante);
+    const estudiantes = await Estudiante.findById(req.params.id);
+    if (!estudiantes) return res.status(404).json({ error: 'Estudiante no encontrado' });
+    res.json(estudiantes);
   } catch (err) {
     res.status(500).json({ error: 'Error al obtener estudiante' });
   }
@@ -45,13 +39,13 @@ router.post('/estudiantes', async (req, res) => {
 // PUT: Actualizar estudiantes por ID
 router.put('/estudiantes/:id', async (req, res) => {
   try {
-    const Estudianteactualizado = await Estudiante.findByIdAndUpdate(
+    const estudianteActualizado = await Estudiante.findByIdAndUpdate(
       req.params.id,
       req.body,
       { new: true, runValidators: true }
     );
-    if (!Estudianteactualizado) return res.status(404).json({ error: 'Estudiante no encontrado' });
-    res.json(Estudianteactualizado);
+    if (!estudianteActualizado) return res.status(404).json({ error: 'Estudiante no encontrado' });
+    res.json(estudianteActualizado);
   } catch (err) {
     res.status(400).json({ error: 'Error al actualizar estudiante' });
   }
@@ -60,8 +54,8 @@ router.put('/estudiantes/:id', async (req, res) => {
 // DELETE: eliminar estudiantes por ID
 router.delete('/estudiantes/:id', async (req, res) => {
   try {
-    const Estudianteeliminado = await Estudiante.findByIdAndDelete(req.params.id);
-    if (!Estudianteeliminado) return res.status(404).json({ error: 'Estudiante no encontrado' });
+    const estudianteEliminado = await Estudiante.findByIdAndDelete(req.params.id);
+    if (!estudianteEliminado) return res.status(404).json({ error: 'Estudiante no encontrado' });
     res.status(204).send();
   } catch (err) {
     res.status(400).json({ error: 'ID incorrecto' });
