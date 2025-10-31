8a2d8ec86ad6499aa0579830adfb9f3dd6c642de
}Camila Aparicio <camy47lg@gmail.com>
2025-10-31 01:56:51 -0300

    modificación de ruta localhost

--- config/corsOptions.js
+++ config/corsOptions.js
@@ -1,5 +1,5 @@
 const corsOptions = {
-  origin: 'http://localhost:3000',
+  origin: 'http://localhost:5173',
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true,
