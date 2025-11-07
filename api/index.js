cb239cc50a7e774499fff86f6ac51a75a215ce39
}Camila Aparicio <camy47lg@gmail.com>
2025-11-07 03:04:17 -0300

    Modificaciónde carpeta indice

--- api/index.js
+++ api/index.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const cors = require('cors');
 const dotenv = require('dotenv');
-const connectDB = require('./config/db');
-const estudiantesRoutes = require('./routes/estudiantes');
-const corsOptions = require('./config/corsOptions');
+const connectDB = require('../config/db');
+const estudiantesRoutes = require('../routes/estudiantes');
+const corsOptions = require('../config/corsOptions');
 
 dotenv.config();
 connectDB();
