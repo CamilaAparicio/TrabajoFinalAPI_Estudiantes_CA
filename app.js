aace44e78c78637acbe008bacf182ecb9f3f409a
}Camila Aparicio <camy47lg@gmail.com>
2025-11-07 02:12:56 -0300

    Mensaje de express

--- app.js
+++ app.js
@@ -20,6 +20,10 @@ app.get('/', (req, res) => {
 
 app.use('/api', estudiantesRoutes);
 
+app.use((req, res, next) => {
+    res.status(404).json({ error: 'Ruta no encontrada' });
+});
+
 app.listen(PORT, () => {
   console.log(`Servidor corriendo en http://localhost:${PORT}`);
 });
