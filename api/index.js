ff0f882c04453e3e80b9bf8a75004c9a42ee42da
}Camila Aparicio <camy47lg@gmail.com>
2025-11-07 02:58:07 -0300

    Modificaciónde carpeta indice

--- api/index.js
+++ api/index.js
@@ -23,8 +23,4 @@ app.use('/api', estudiantesRoutes);
 app.use((req, res, next) => {
     res.status(404).json({ error: 'Ruta no encontrada' });
 });
-
-app.listen(PORT, () => {
-  console.log(`Servidor corriendo en http://localhost:${PORT}`);
-});
-
+module.exports = app;
